de570142d749f36c6c16c414f2abdcbbc627e71c
David Streever <dstreever@hortonworks.com>
2013-10-21 19:37:09 -0400

    Better Parameters

--- oozie-workflows/control.properties/workflows/run-wf.sh
+++ oozie-workflows/control.properties/workflows/run-wf.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-if [ $# < 3 ]; then
-  echo "Need to specify a Workflow, Job and an email address for notifications"
+if [ $# < 2 ]; then
+  echo "Need to specify a Workflow, an email address, and job (optional) for notifications"
   exit -1;
 fi
 
+WORKFLOW=$1
+EMAIL=$2
+
+# Default to "job"
+JOB=${3:-"job"}
+
 # HDFS hive-site.xml location
-HIVE_SITE=${4:-"/hdp_1.3.2/share/lib/conf/hive-site.xml"}
+HIVE_SITE=${4:-"/user/oozie/share/lib/conf/hive-site.xml"}
 
 APP_PATH=`dirname $0`
+
 echo $APP_PATH
 
 cd $APP_PATH
 
-. $APP_PATH/submit.sh $1
+. $APP_PATH/submit.sh $WORKFLOW
+
+# This can be set by a global variable
+OOZIE_URL=http://localhost:11000/oozie
 
 # Submit and Start the job with one call.  To split it out, use submit then start.
-oozie job -oozie http://localhost:11000/oozie -config $1/$2.properties -run -verbose -Demailto=$3 -Duser=$USER -Dhive.site=$HIVE_SITE
+oozie job -config $WORKFLOW/$JOB.properties -run -verbose -Demailto=$EMAIL -Duser=$USER -Dhive.site=$HIVE_SITE
